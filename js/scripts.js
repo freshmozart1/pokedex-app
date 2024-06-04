06391514416d19755e29db855f998f5a155f74b8
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-06-04 14:43:27 +0200

    I added the index.html and scripts.js file.

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+const favoriteFood = 'Sushi';
+
+window.alert('hello, world'); /*The original hello-world-code from 'Programming in C - A Tutorial (1974) was 'main() {printf("hello, world");}'*/
+document.querySelector('#favoriteFood').innerHTML = favoriteFood;
