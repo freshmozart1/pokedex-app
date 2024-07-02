cd70b5ff33fc7790ad053b9379e15d15b72f47d7
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-07-02 17:02:08 +0200

    I added basic search functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
 jQuery(() => {
+  const pokemons = [];
+  const searchButton = $('#searchButton');
+  const clearSearchButton = $('#clearSearchButton');
+  const searchField = $('#searchField');
+  const pokemonNotFoundMessage = $('#noPokemonsFound');
+
   /**
    * This class represents a Pokemon.
    */
@@ -46,7 +52,7 @@ jQuery(() => {
       );
       cardButton.on("click", () => this.#setModalContent());
       $("#pokemonContainer").append(
-        $('<div class="col pt-3"></div>').append(
+        $(`<div class="col pt-3" pokemon="${this.name.toLowerCase()}"></div>`).append(
           $('<div class="card border-0"></div>').append(
             $(
               '<img class="card-img" alt="' +
@@ -74,18 +80,68 @@ jQuery(() => {
               throw new Error("HTTP error " + response2.status);
             } else {
               response2.json().then((response2Json) => {
-                new Pokemon(
+                const pokemon = new Pokemon(
                   response2Json.name.charAt(0).toUpperCase() +
                   response2Json.name.slice(1),
                   response2Json.sprites.other["official-artwork"].front_default,
                   response2Json.height,
                   response2Json.types.map((type) => type.type.name)
-                ).addToDOM();
+                );
+                pokemons.push(pokemon);
+                pokemon.addToDOM();
               });
             }
           });
         });
       });
     }
+  });
+
+  // Search functionality
+  searchButton.on('click', (event) => {
+    event.preventDefault();
+    console.log('button clicked');
+    let search = searchField.val();
+    if (search) {
+      searchButton.hide();
+      clearSearchButton.show();
+      let foundPokemon = false;
+      pokemons.forEach((pokemon) => {
+        if (pokemon.name.toLowerCase().includes(search.toLowerCase())) {
+          $(`[pokemon="${pokemon.name.toLowerCase()}"]`).show();
+          pokemonNotFoundMessage.hide();
+          foundPokemon = true;
+        } else {
+          $(`[pokemon="${pokemon.name.toLowerCase()}"]`).hide();
+        }
+      });
+      if (!foundPokemon) {
+        pokemonNotFoundMessage.children('p').text(`No Pokémon found with the name "${search}"`);
+        pokemonNotFoundMessage.show();
+      }
+    } else {
+      /* TODO: When a user loads the page and clicks the search button without entering any text,
+      nothing happens. But when the user enters something, clicks search, then deletes everything
+      and clicks search again, the following message gets shown.*/
+      pokemonNotFoundMessage.children('p').text('Please enter a Pokémon name to search');
+      pokemonNotFoundMessage.show();
+    }
+  });
+
+  // Clear search functionality
+  clearSearchButton.on('click', (event) => {
+    event.preventDefault();
+    searchButton.show();
+    clearSearchButton.hide();
+    searchField.val('');
+    pokemonNotFoundMessage.hide();
+    pokemons.forEach((pokemon) => {
+      $(`[pokemon="${pokemon.name.toLowerCase()}"]`).show();
+    });
+  });
+
+  searchField.on('input', () => {
+    searchButton.show();
+    clearSearchButton.hide();
   });
 });
